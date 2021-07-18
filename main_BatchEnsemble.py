d3b8c7bd1fb8e0347ea6420758b7444f29e3f970
ChengJiacheng <1967436138q@gmail.com>
2021-07-18 12:20:33 -0700

    Update main_BatchEnsemble.py

--- main_BatchEnsemble.py
+++ main_BatchEnsemble.py
@@ -18,28 +18,6 @@ import datetime
 import numpy as np
 from networks import *
 from torch.autograd import Variable
-
-parser = argparse.ArgumentParser(description='PyTorch CIFAR-10 Training')
-parser.add_argument('--lr', default=0.1, type=float, help='learning_rate')
-#parser.add_argument('--net_type', default='wide-resnet', type=str, help='model') # the code works just with wideresnet for now
-parser.add_argument('--depth', default=28, type=int, help='depth of model')
-parser.add_argument('--widen_factor', default=10, type=int, help='width of model')
-parser.add_argument('--dropout', default=0.3, type=float, help='dropout_rate')
-parser.add_argument('--dataset', default='cifar10', type=str, help='dataset = [cifar10/cifar100]')
-parser.add_argument('--dirsave_out', default='batch_ensemble', type=str, help='where the checkpoint are save. ./checkpoint/dataset/dirsave_out')
-parser.add_argument('--resume', '-r', action='store_true', help='resume from checkpoint')
-parser.add_argument('--testOnly', '-t', action='store_true', help='Test mode with the saved model')
-args = parser.parse_args()
-#python main_BatchEnsemble.py  --depth 28
-
-ensemble_size=4
-# Hyper Parameter settings
-use_cuda = torch.cuda.is_available()
-best_acc = 0
-start_epoch, num_epochs, batch_size, optim_type = cf.start_epoch, cf.num_epochs, cf.batch_size, cf.optim_type
-batch_size=128#128
-# Data Uplaod
-cutout=16
 class CutoutDefault(object):
     """
     Reference : https://github.com/quark0/darts/blob/master/cnn/utils.py
@@ -66,36 +44,6 @@ class CutoutDefault(object):
         img *= mask
         return img
 
-print('\n[Phase 1] : Data Preparation')
-transform_train = transforms.Compose([
-    transforms.RandomCrop(32, padding=4),
-    transforms.RandomHorizontalFlip(),
-    transforms.ToTensor(),
-    transforms.Normalize(cf.mean[args.dataset], cf.std[args.dataset]),
-    CutoutDefault(cutout)
-]) # meanstd transformation
-
-transform_test = transforms.Compose([
-    transforms.ToTensor(),
-    transforms.Normalize(cf.mean[args.dataset], cf.std[args.dataset]),
-])
-
-if(args.dataset == 'cifar10'):
-    print("| Preparing CIFAR-10 dataset...")
-    sys.stdout.write("| ")
-    trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform_train)
-    testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=False, transform=transform_test)
-    num_classes = 10
-elif(args.dataset == 'cifar100'):
-    print("| Preparing CIFAR-100 dataset...")
-    sys.stdout.write("| ")
-    trainset = torchvision.datasets.CIFAR100(root='./data', train=True, download=True, transform=transform_train)
-    testset = torchvision.datasets.CIFAR100(root='./data', train=False, download=False, transform=transform_test)
-    num_classes = 100
-
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=2)
-testloader = torch.utils.data.DataLoader(testset, batch_size=50, shuffle=False, num_workers=2)
-
 # Return network & file name
 def getNetwork(args):
     # if you want to implement more architectures
@@ -103,82 +51,6 @@ def getNetwork(args):
     file_name = 'wide-resnet-'+str(args.depth)+'x'+str(args.widen_factor)
 
     return net, file_name
-
-# Test only option
-if (args.testOnly):
-    print('\n[Test Phase] : Model setup')
-    assert os.path.isdir('checkpoint'), 'Error: No checkpoint directory found!'
-    _, file_name = getNetwork(args)
-    checkpoint = torch.load('./checkpoint/'+args.dataset+os.sep+file_name+'.t7')
-    net = checkpoint['net']
-
-    if use_cuda:
-        net.cuda()
-        net = torch.nn.DataParallel(net, device_ids=range(torch.cuda.device_count()))
-        cudnn.benchmark = True
-
-    net.eval()
-    net.training = False
-    test_loss = 0
-    correct = 0
-    total = 0
-
-    with torch.no_grad():
-        for batch_idx, (inputs, targets) in enumerate(testloader):
-            if use_cuda:
-                inputs, targets = inputs.cuda(), targets.cuda()
-            inputs, targets = Variable(inputs), Variable(targets)
-            outputs = net(inputs)
-
-            _, predicted = torch.max(outputs.data, 1)
-            total += targets.size(0)
-            correct += predicted.eq(targets.data).cpu().sum()
-
-        acc = 100.*correct/total
-        print("| Test Result\tAcc@1: %.2f%%" %(acc))
-
-    sys.exit(0)
-
-# Model
-print('\n[Phase 2] : Model setup')
-if args.resume:
-    # Load checkpoint
-    print('| Resuming from checkpoint...')
-    assert os.path.isdir('checkpoint'), 'Error: No checkpoint directory found!'
-    _, file_name = getNetwork(args)
-    checkpoint = torch.load('./checkpoint/'+args.dataset+os.sep+file_name+'.t7')
-    net = checkpoint['net']
-    best_acc = checkpoint['acc']
-    start_epoch = checkpoint['epoch']
-else:
-    print('| Building net type [wide-resnet BE]...')
-    net, file_name = getNetwork(args)
-    #net.apply(conv_init)
-
-if use_cuda:
-    net.cuda()
-    net = torch.nn.DataParallel(net, device_ids=range(torch.cuda.device_count()))
-    cudnn.benchmark = True
-
-criterion = nn.CrossEntropyLoss()
-
-# Training
-def tile(a, dim, n_tile):
-    init_dim = a.size(dim)
-    repeat_idx = [1] * a.dim()
-    repeat_idx[dim] = n_tile
-    a = a.repeat(*(repeat_idx))
-    order_index = torch.LongTensor(np.concatenate([init_dim * np.arange(n_tile) + i for i in range(init_dim)]))
-    return torch.index_select(a, dim, order_index)
-
-my_list = ['alpha', 'gamma']
-
-
-
-params_multi_tmp= list(filter(lambda kv: (my_list[0] in kv[0]) or (my_list[1] in kv[0]) , net.named_parameters()))
-param_core_tmp = list(filter(lambda kv: (my_list[0] not in kv[0]) and (my_list[1] not in kv[0]), net.named_parameters()))
-params_multi=[param for name, param in params_multi_tmp]
-param_core=[param for name, param in param_core_tmp]
 
 def train(epoch):
     net.train()
@@ -259,23 +131,163 @@ def test(epoch):
             torch.save(state, save_point+file_name+'.t7')
             best_acc = acc
 
-print('\n[Phase 3] : Training model')
-print('| Training Epochs = ' + str(num_epochs))
-print('| Initial Learning Rate = ' + str(args.lr))
-print('| Optimizer = ' + str(optim_type))
-for name, param in net.named_parameters():
-    print('name', name)
 
-elapsed_time = 0
-for epoch in range(start_epoch, start_epoch+num_epochs):
-    start_time = time.time()
+def tile(a, dim, n_tile):
+    init_dim = a.size(dim)
+    repeat_idx = [1] * a.dim()
+    repeat_idx[dim] = n_tile
+    a = a.repeat(*(repeat_idx))
+    order_index = torch.LongTensor(np.concatenate([init_dim * np.arange(n_tile) + i for i in range(init_dim)]))
+    return torch.index_select(a, dim, order_index)
 
-    train(epoch)
-    test(epoch)
-
-    epoch_time = time.time() - start_time
-    elapsed_time += epoch_time
-    print('| Elapsed time : %d:%02d:%02d'  %(cf.get_hms(elapsed_time)))
-
-print('\n[Phase 4] : Testing model')
-print('* Test results : Acc@1 = %.2f%%' %(best_acc))
+if __name__ == '__main__':
+        
+    parser = argparse.ArgumentParser(description='PyTorch CIFAR-10 Training')
+    parser.add_argument('--lr', default=0.1, type=float, help='learning_rate')
+    #parser.add_argument('--net_type', default='wide-resnet', type=str, help='model') # the code works just with wideresnet for now
+    parser.add_argument('--depth', default=28, type=int, help='depth of model')
+    parser.add_argument('--widen_factor', default=10, type=int, help='width of model')
+    parser.add_argument('--dropout', default=0.3, type=float, help='dropout_rate')
+    parser.add_argument('--dataset', default='cifar10', type=str, help='dataset = [cifar10/cifar100]')
+    parser.add_argument('--dirsave_out', default='batch_ensemble', type=str, help='where the checkpoint are save. ./checkpoint/dataset/dirsave_out')
+    parser.add_argument('--resume', '-r', action='store_true', help='resume from checkpoint')
+    parser.add_argument('--testOnly', '-t', action='store_true', help='Test mode with the saved model')
+    args = parser.parse_args()
+    #python main_BatchEnsemble.py  --depth 28
+    
+    
+    ensemble_size=4
+    # Hyper Parameter settings
+    use_cuda = torch.cuda.is_available()
+    best_acc = 0
+    start_epoch, num_epochs, batch_size, optim_type = cf.start_epoch, cf.num_epochs, cf.batch_size, cf.optim_type
+    # batch_size=128#128
+    batch_size=16
+    # Data Uplaod
+    cutout=16
+    
+    
+    print('\n[Phase 1] : Data Preparation')
+    transform_train = transforms.Compose([
+        transforms.RandomCrop(32, padding=4),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize(cf.mean[args.dataset], cf.std[args.dataset]),
+        CutoutDefault(cutout)
+    ]) # meanstd transformation
+    
+    transform_test = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize(cf.mean[args.dataset], cf.std[args.dataset]),
+    ])
+    
+    if(args.dataset == 'cifar10'):
+        print("| Preparing CIFAR-10 dataset...")
+        sys.stdout.write("| ")
+        trainset = torchvision.datasets.CIFAR10(root='\datasets', train=True, download=True, transform=transform_train)
+        testset = torchvision.datasets.CIFAR10(root='\dataset', train=False, download=True, transform=transform_test)
+        num_classes = 10
+    elif(args.dataset == 'cifar100'):
+        print("| Preparing CIFAR-100 dataset...")
+        sys.stdout.write("| ")
+        trainset = torchvision.datasets.CIFAR100(root='\dataset', train=True, download=True, transform=transform_train)
+        testset = torchvision.datasets.CIFAR100(root='\dataset', train=False, download=True, transform=transform_test)
+        num_classes = 100
+    
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=2)
+    testloader = torch.utils.data.DataLoader(testset, batch_size=50, shuffle=False, num_workers=2)
+    
+    
+    
+    # Test only option
+    if (args.testOnly):
+        print('\n[Test Phase] : Model setup')
+        assert os.path.isdir('checkpoint'), 'Error: No checkpoint directory found!'
+        _, file_name = getNetwork(args)
+        checkpoint = torch.load('./checkpoint/'+args.dataset+os.sep+file_name+'.t7')
+        net = checkpoint['net']
+    
+        if use_cuda:
+            net.cuda()
+            net = torch.nn.DataParallel(net, device_ids=range(torch.cuda.device_count()))
+            cudnn.benchmark = True
+    
+        net.eval()
+        net.training = False
+        test_loss = 0
+        correct = 0
+        total = 0
+    
+        with torch.no_grad():
+            for batch_idx, (inputs, targets) in enumerate(testloader):
+                if use_cuda:
+                    inputs, targets = inputs.cuda(), targets.cuda()
+                inputs, targets = Variable(inputs), Variable(targets)
+                outputs = net(inputs)
+    
+                _, predicted = torch.max(outputs.data, 1)
+                total += targets.size(0)
+                correct += predicted.eq(targets.data).cpu().sum()
+    
+            acc = 100.*correct/total
+            print("| Test Result\tAcc@1: %.2f%%" %(acc))
+    
+        sys.exit(0)
+    
+    # Model
+    print('\n[Phase 2] : Model setup')
+    if args.resume:
+        # Load checkpoint
+        print('| Resuming from checkpoint...')
+        assert os.path.isdir('checkpoint'), 'Error: No checkpoint directory found!'
+        _, file_name = getNetwork(args)
+        checkpoint = torch.load('./checkpoint/'+args.dataset+os.sep+file_name+'.t7')
+        net = checkpoint['net']
+        best_acc = checkpoint['acc']
+        start_epoch = checkpoint['epoch']
+    else:
+        print('| Building net type [wide-resnet BE]...')
+        net, file_name = getNetwork(args)
+        #net.apply(conv_init)
+    
+    if use_cuda:
+        net.cuda()
+        net = torch.nn.DataParallel(net, device_ids=range(torch.cuda.device_count()))
+        cudnn.benchmark = True
+    
+    criterion = nn.CrossEntropyLoss()
+    
+    # Training
+    
+    
+    my_list = ['alpha', 'gamma']
+    
+    
+    
+    params_multi_tmp= list(filter(lambda kv: (my_list[0] in kv[0]) or (my_list[1] in kv[0]) , net.named_parameters()))
+    param_core_tmp = list(filter(lambda kv: (my_list[0] not in kv[0]) and (my_list[1] not in kv[0]), net.named_parameters()))
+    params_multi=[param for name, param in params_multi_tmp]
+    param_core=[param for name, param in param_core_tmp]
+    
+    
+    
+    print('\n[Phase 3] : Training model')
+    print('| Training Epochs = ' + str(num_epochs))
+    print('| Initial Learning Rate = ' + str(args.lr))
+    print('| Optimizer = ' + str(optim_type))
+    for name, param in net.named_parameters():
+        print('name', name)
+    
+    elapsed_time = 0
+    for epoch in range(start_epoch, start_epoch+num_epochs):
+        start_time = time.time()
+    
+        train(epoch)
+        test(epoch)
+    
+        epoch_time = time.time() - start_time
+        elapsed_time += epoch_time
+        print('| Elapsed time : %d:%02d:%02d'  %(cf.get_hms(elapsed_time)))
+    
+    print('\n[Phase 4] : Testing model')
+    print('* Test results : Acc@1 = %.2f%%' %(best_acc))
